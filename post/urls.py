c39470f9caaf277232114e0a6d40bd6551827efe
osm3rr <osm3rr@mail.com>
2024-11-02 15:27:47 -0400

    Holaaaa

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import HomePageView,UsuariosView
+from .views import HomePageView,UsuariosView, UpdatePageView, DeletePageView
+from post.views import CreateView,DetailPageView
 
 urlpatterns= [
     path("", HomePageView.as_view(), name = "post"),
-    path("usuarios",UsuariosView.as_view(),name="usuarios") 
-
-]
-
+    path("usuarios",UsuariosView.as_view(),name="usuarios"),
+    path("post/create",CreateView.as_view(),name="create"),
+    path("post/detail/<int:pk>",DetailPageView.as_view(),name="detail"),
+    ##Aqui
+    path("post/detail/<int:pk>/update",UpdatePageView.as_view(),name="update"),
+    path("post/detail/<int:pk>",DetailPageView.as_view(),name="detail"),
+    path("post/detail/<int:pk>/delete",DeletePageView.as_view(),name="delete"),
+    ]
--- post/views.py
+++ post/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post, Personas
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.views.generic.edit import DeleteView,UpdateView
 
-# Create your views here.
-
+# Create your views here.   
 class HomePageView(ListView):
     template_name = "post.html"
     model = Post
@@ -11,3 +14,27 @@ class HomePageView(ListView):
 class UsuariosView(ListView):
     template_name = "usuarios.html"
     model = Personas
+
+class CreateView(CreateView):
+
+    model=Post
+    template_name="create.html"
+    fields=["titulo","descripcion","imagen","precio"]
+    success_url = reverse_lazy("post")
+
+class DetailPageView(DetailView):
+    model=Post
+    template_name="detail.html"
+
+class UpdatePageView(UpdateView):
+    template_name="update.html"
+    model=Post
+    fields=["titulo","descripcion","imagen","precio"]
+    success_url=reverse_lazy("post")
+
+class DeletePageView(DeleteView):
+    
+    template_name="delete.html"
+    model=Post
+    success_url=reverse_lazy("post")
+
